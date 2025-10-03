869ad85c6d833de1377d7f44902d34ee26caf550
leonardo rodrigues <leonardopessanharodrigues@gmail.com>
2025-10-03 20:10:15 -0300

    variaveis da classe student criadas

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        int test = 10;
-        System.out.println(test);
+
     }
 }
--- src/entities/Student.java
+++ src/entities/Student.java
@@ -1,4 +1,8 @@
 package entities;
 
 public class Student {
+    public String name;
+    public short _1quarterNote;
+    public short _2quarterNote;
+    public short _3quarterNote;
 }
